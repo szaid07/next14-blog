35fa4d18c8cd19b4e56f38ecf50847f9aa98f934
Shaikh Zaid <szaid07@gmail.com>
2025-05-10 11:08:52 +0400

    Data Fetching and chahing

--- src/app/about/projects/page.tsx
+++ src/app/about/projects/page.tsx
@@ -1,3 +1,23 @@
-export default function ProjectsPage() {
-  return <>Projects!</>;
+export const dynamic = "force-dynamic";
+
+export default async function ProjectsPage() {
+  const response = await fetch("http://localhost:3001/repos", {
+    next: { revalidate: 3600 },
+  });
+  const repos = await response.json();
+  return (
+    <div className="p-20">
+      <h1 className="mb-8 text-xl">Projects</h1>
+
+      <ul>
+        {repos.map((repo) => (
+          <li key={repo.id} className="mb-8">
+            <div>{repo.title}</div>
+            <div>{repo.description}</div>
+            <div>{repo.stargazers_count}</div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
